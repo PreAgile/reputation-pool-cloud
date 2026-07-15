3b06b147a5b39a41da392d96356911ed35c392ae
Meyon Soo  Kim <39788337+PreAgile@users.noreply.github.com>
2026-07-15 18:37:01 +0900

    build: scaffold Spring Boot cloud application

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.3"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.diffplug.spotless") version "7.0.4"
+}
+
+group = "io.github.preagile"
+version = "0.1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+extra["grpcSpringBootStarterVersion"] = "3.1.0.RELEASE"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("net.devh:grpc-spring-boot-starter:${property("grpcSpringBootStarterVersion")}")
+    implementation("io.github.preagile:reputation-pool-core:0.2.0")
+    implementation("io.github.preagile:reputation-pool-persistence:0.2.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+spotless {
+    kotlinGradle {
+        ktlint()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    java {
+        palantirJavaFormat()
+        removeUnusedImports()
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
---
+++ src/test/java/io/github/preagile/reputationpool/cloud/ReputationPoolCloudApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.preagile.reputationpool.cloud;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReputationPoolCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertThat(true).isTrue();
+    }
+}
